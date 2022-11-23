5745a2d536ee262b42bddf9c60fd99f989c49a40
Ernesto Miranda <ernestolebni123@gmail.com>
2022-11-23 15:07:19 -0600

    env added

--- src/App/modules/integration/integration.module.ts
+++ src/App/modules/integration/integration.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CalendarClient } from './client/calendar.client';
 import { IntegrationController } from './integration.controller';
 
